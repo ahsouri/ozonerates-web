c2dc22f5e15b3ca73d01952077da995df834a292
Amir Souri <ahsouri@gmail.com>
2025-04-08 14:13:23 -0400

    fixed the meta

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,7 +1,7 @@
 import ogImageSrc from "@images/ozonerates_logo.png";
 
 export const SITE = {
-  title: "Ozonerates",
+  title: "Explore Ozonerates From Space",
   tagline: "Estimating PO3 from Space",
   description: "We have established a robust relationship between ozone production rates and several geophysical parameters obtained from several intensive atmospheric composition campaigns. We have shown that satellite remote sensing data can effectively constrain these parameters, enabling us to produce the first global maps of ozone production rates with unprecedented resolution.",
   description_short: "We produce global maps of PO3 determined from space.",
@@ -33,7 +33,7 @@ export const OG = {
   locale: "en_US",
   type: "website",
   url: SITE.url,
-  title: `${SITE.title}: : Official Website to Get Informed About PO3 Products Determined from Space`,
+  title: `${SITE.title}: : Get Informed About PO3 Products Determined from Space`,
   description: "We have established a robust relationship between ozone production rates and several geophysical parameters obtained from several intensive atmospheric composition campaigns. We have shown that satellite remote sensing data can effectively constrain these parameters, enabling us to produce the first global maps of ozone production rates with unprecedented resolution.",
   image: ogImageSrc,
 };
